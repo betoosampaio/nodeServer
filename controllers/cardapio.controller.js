18a9330e03445b4374bee18d112259d519c9066e
iagoguimaraes <iagog6@hotmail.com>
2019-09-11 16:33:49 -0300

    4 primeiros controllers

--- controllers/cardapio.controller.js
+++ controllers/cardapio.controller.js
@@ -2,7 +2,7 @@ const database = require('../config/database.config')
 
 module.exports.selectAll = async (req,res) => {
     try {
-        let data = await database.query("select * from TB_RESTAURANTE");
+        let data = await database.query("select * from TB_CARDAPIO");
         res.json(data);
     } catch (error) {
         throw error;
@@ -11,7 +11,7 @@ module.exports.selectAll = async (req,res) => {
 
 module.exports.insert = async (req,res) => {
     try {
-        let query = "insert into TB_RESTAURANTE() values (?)";
+        let query = "insert into TB_CARDAPIO() values (?)";
         await database.query(query, []);
         res.json('OK');
     } catch (error) {
@@ -21,8 +21,8 @@ module.exports.insert = async (req,res) => {
 
 module.exports.update = async (req,res) => {
     try {
-        let query = "insert into TB_RESTAURANTE() values (?)";
-        await database.query(query, [req.body.id_restaurante]);
+        let query = "insert into TB_CARDAPIO() values (?)";
+        await database.query(query, [req.body.id_cardapio]);
         res.json('OK');
     } catch (error) {
         throw error;
@@ -31,8 +31,8 @@ module.exports.update = async (req,res) => {
 
 module.exports.delete = async (req,res) => {
     try {
-        let query = "update TB_RESTAURANTE set ativo = 0, data_exclusao = now() where id_restaurante = ?";
-        await database.query(query, [req.body.id_restaurante]);
+        let query = "update TB_CARDAPIO set ativo = 0, data_exclusao = now() where id_cardapio = ?";
+        await database.query(query, [req.body.id_cardapio]);
         res.json('OK');
     } catch (error) {
         throw error;
--- routes.js
+++ routes.js
@@ -1,6 +1,27 @@
 module.exports = (app) => {
-    const teste = require('./controllers/teste.controller');
+    const restaurante = require('./controllers/restaurante.controller');
+    const cardapio = require('./controllers/cardapio.controller');
+    const operador = require('./controllers/operador.controller');
+    const menu = require('./controllers/menu.controller');
 
-    app.get('/', teste.getAll);
-    app.get('/insere', teste.insere);
+    app.post('/restaurante/selectall', restaurante.selectAll);
+    app.post('/restaurante/insert', restaurante.insert);
+    app.post('/restaurante/update', restaurante.update);
+    app.post('/restaurante/delete', restaurante.delete);
+
+    app.post('/cardapio/selectall', cardapio.selectAll);
+    app.post('/cardapio/insert', cardapio.insert);
+    app.post('/cardapio/update', cardapio.update);
+    app.post('/cardapio/delete', cardapio.delete);
+
+    app.post('/operador/selectall', operador.selectAll);
+    app.post('/operador/insert', operador.insert);
+    app.post('/operador/update', operador.update);
+    app.post('/operador/delete', operador.delete);
+
+    app.post('/menu/selectall', menu.selectAll);
+    app.post('/menu/insert', menu.insert);
+    app.post('/menu/update', menu.update);
+    app.post('/menu/delete', menu.delete);
+    
 }
