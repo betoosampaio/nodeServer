8dfb0984d312d7645757e38cbbda8a0f480ebf99
iagoguimaraes <iagog6@hotmail.com>
2019-09-11 18:47:56 -0300

    ajuste no database.config

--- config/database.config.js
+++ config/database.config.js
@@ -1,21 +1,17 @@
 const mariadb = require('mariadb');
 
-const pool = mariadb.createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'db_creed',
-});
-
-module.exports.query = async (query) => {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        return await conn.query(query);
-    } catch (error) {
+module.exports.query = (query, params) => {
+    mariadb.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'db_creed',
+    }).then(conn => {
+        return conn.query(query, params);
+    }).catch(error =>{
         throw error;
-    } finally {
-        if (conn) conn.end();
-    }
-}
+    }).catch(error => {
+        throw error;
+    });
+};
--- controllers/restaurante.controller.js
+++ controllers/restaurante.controller.js
@@ -1,6 +1,6 @@
 const database = require('../config/database.config')
 
-module.exports.selectAll = async (req,res) => {
+module.exports.selectAll = async (req, res) => {
     try {
         let data = await database.query("select * from TB_RESTAURANTE");
         res.json(data);
@@ -9,17 +9,18 @@ module.exports.selectAll = async (req,res) => {
     }
 }
 
-module.exports.insert = async (req,res) => {
+module.exports.insert = (req, res) => {
     try {
-        let query = "insert into TB_RESTAURANTE() values (?)";
-        await database.query(query, []);
+        let obj = req.body;
+        let query = "insert into tb_teste(nome, sobrenome) values (?, ?)";
+        database.query(query, [obj.nome, obj.sobrenome]);
         res.json('OK');
     } catch (error) {
         throw error;
     }
 }
 
-module.exports.update = async (req,res) => {
+module.exports.update = async (req, res) => {
     try {
         let query = "insert into TB_RESTAURANTE() values (?)";
         await database.query(query, [req.body.id_restaurante]);
@@ -29,7 +30,7 @@ module.exports.update = async (req,res) => {
     }
 }
 
-module.exports.delete = async (req,res) => {
+module.exports.delete = async (req, res) => {
     try {
         let query = "update TB_RESTAURANTE set ativo = 0, data_exclusao = now() where id_restaurante = ?";
         await database.query(query, [req.body.id_restaurante]);
