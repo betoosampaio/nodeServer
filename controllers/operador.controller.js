2318f953e2927642ab9d5e16638490aa5fe528ed
Diego <honorioproduction@gmail.com>
2019-09-17 12:32:28 -0300

    Validações dos campos

--- controllers/operador.controller.js
+++ controllers/operador.controller.js
@@ -1,41 +1,72 @@
 const database = require('../config/database.config')
 
-module.exports.selectAll = (req,res) => {
+module.exports.selectAll = (req, res) => {
     try {
-        let data = database.query("select * from cadastro_operador");
-        res.json({data});
+        let data = database.query("select * from tb_operador");
+        res.json(data);
     } catch (error) {
         throw error;
     }
 }
 
-module.exports.insert = (req,res) => {
+
+
+module.exports.insert = (req, res) => {
     try {
+
         let obj = req.body;
-        console.log(obj);
-        let query = "insert into cadastro_operador(nome_Operador, perfil, login_Operador, senha_Operador ) values (?,?,?,?)";
-        database.query(query, [obj.nome_Operador, obj.perfil, obj.login_Operador, obj.senha_Operador]);
 
+        if(obj.nome_Operador == ''){
+            console.log('Falta o Nome do Operador a ser preenchidos.')
+        }
+       else if(obj.perfil == ''){
+            console.log('Falta o perfil a ser preenchidos.')
+        }
+        else if(obj.login_Operador == ''){
+            console.log('Falta o Login Operador a ser preenchidos.')
+        }
+        else if(obj.senha_Operador == ''){
+            console.log('Falta o Senha Operador a ser preenchidos.')
+        }
+
+        else{
+              console.log(obj);
+        let query = `insert into tb_operador(
+            nome_Operador,
+            perfil,
+            login_Operador,
+            senha_Operador)
+            values (?,?,?,?)`;
+
+
+        database.query(query, [
+            obj.nome_Operador,
+            obj.perfil,
+            obj.login_Operador,
+            obj.senha_Operador,
+        ]);
+    }
+        res.json('OK');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+
+module.exports.update = (req, res) => {
+    try {
+        let query = "insert into TB_RESTAURANTE() values (?)";
+        database.query(query, [req.body.id_restaurante]);
         res.json('OK');
     } catch (error) {
         throw error;
     }
 }
 
-module.exports.update = (req,res) => {
+module.exports.delete = (req, res) => {
     try {
-        let query = "insert into TB_OPERADOR() values (?)";
-     database.query(query, [req.body.id_operador]);
-        res.json('OK');
-    } catch (error) {
-        throw error;
-    }
-}
-
-module.exports.delete = (req,res) => {
-    try {
-        let query = "update TB_OPERADOR set ativo = 0, data_exclusao = now() where id_operador = ?";
-     database.query(query, [req.body.id_operador]);
+        let query = "update TB_RESTAURANTE set ativo = 0, data_exclusao = now() where id_restaurante = ?";
+        database.query(query, [req.body.id_restaurante]);
         res.json('OK');
     } catch (error) {
         throw error;
--- routes.js
+++ routes.js
@@ -2,18 +2,22 @@ module.exports = (app) => {
 
     app.get('/', (req, res) =>{ res.json("Server Online") });
 
+
+    /* ROTAS PARA A PAGINA RESTAURANTE */
     const restaurante = require('./controllers/restaurante.controller');
     app.post('/restaurante/selectall', restaurante.selectAll);
     app.post('/restaurante/insert', restaurante.insert);
     app.post('/restaurante/update', restaurante.update);
     app.post('/restaurante/delete', restaurante.delete);
 
+    /* ROTAS PARA A PAGINA CARDÁPIO */
     const cardapio = require('./controllers/cardapio.controller');
     app.post('/cardapio/selectall', cardapio.selectAll);
     app.post('/cardapio/insert', cardapio.insert);
     app.post('/cardapio/update', cardapio.update);
     app.post('/cardapio/delete', cardapio.delete);
 
+    /* ROTAS PARA A PAGINA OPERADOR */
     const operador = require('./controllers/operador.controller');
     app.get('/operador/selectall', operador.selectAll);
     app.post('/operador/insert', operador.insert);
