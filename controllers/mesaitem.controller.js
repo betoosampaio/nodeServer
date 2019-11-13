ae0fd242788f9bfbf96642ba0974ad7ac6ee9311
iagoguimaraes <iagog6@hotmail.com>
2019-11-12 21:46:04 -0300

    validar mesa fechada items e pagamentos

--- controllers/mesaitem.controller.js
+++ controllers/mesaitem.controller.js
@@ -4,92 +4,113 @@ const ObjectId = require('../utils/mongodb.util').ObjectId;
 const model = require('../models/mesaitem.model');
 const produtoCtrl = require('../controllers/produto.controller');
 const operadorCtrl = require('../controllers/operador.controller');
+const mesaCtrl = require('../controllers/mesa.controller');
 
 module.exports.incluir = async (req, res) => {
-    try {
-        // recupera os dados da requisição
-        let obj = {
-            id_mesa: req.body.id_mesa,
-            produtos: req.body.produtos,
-            id_operador: req.body.id_operador || req.token.id_operador,
-        }
-
-        // validação dos dados da requisição
-        let errors = model.validarIncluir(obj);
-        if (errors)
-            return res.status(400).send(errors[0]);
-        if (!obj.produtos || obj.produtos.length == 0)
-            return res.status(400).send("Não há produtos na lista");
-
-        let operador = await operadorCtrl._obter(req.token.id_restaurante, obj.id_operador);
-        if(operador.length == 0)
-            return res.status(400).send("Operador inválido");
-               
-        // obtendo os dados dos produtos
-        let produtos = [];
-        for (p of obj.produtos) {
-            // validando o item
-            let errors = model.validarItem(p);
-            if (errors)
-                return res.status(400).send(errors[0]);
-            // obtendo os dados do produto
-            let produto = await produtoCtrl._obter(req.token.id_restaurante, p.id_produto);
-            if (!produto || produto.length == 0)
-                return res.status(400).send("produto(s) inválido(s)");
-            else {
-                produto = produto[0];
-                // inclui a quantidade e atribui um id e dados da inserção
-                produto.quantidade = p.quantidade;
-                produto.id_item = new ObjectId();
-                produto.data_inclusao = new Date();
-                produto.id_operador = operador[0].id_operador;
-                produto.nome_operador = operador[0].nome_operador;
-                produtos.push(produto);
-            }
-        }
-
-        // incluindo os produtos na mesa
-        await mongodb.updateOne('freeddb', 'mesa',
-            { _id: new ObjectId(obj.id_mesa), id_restaurante: req.token.id_restaurante },
-            { $push: { produtos: { $each: produtos } } }
-        );
-
-        return res.json('OK');
-    } catch (error) {
-        return res.status(500).send(error.message);
+  try {
+    // recupera os dados da requisição
+    let obj = {
+      id_mesa: req.body.id_mesa,
+      produtos: req.body.produtos,
+      id_operador: req.body.id_operador || req.token.id_operador,
     }
+
+    // validação dos dados da requisição
+    let errors = model.validarIncluir(obj);
+    if (errors)
+      return res.status(400).send(errors[0]);
+    if (!obj.produtos || obj.produtos.length == 0)
+      return res.status(400).send("Não há produtos na lista");
+
+    // validar operador
+    let operador = await operadorCtrl._obter(req.token.id_restaurante, obj.id_operador);
+    if (operador.length == 0)
+      return res.status(400).send("Operador inválido");
+
+    // obtem mesa
+    let mesa = await mesaCtrl._obter(req.token.id_restaurante, req.body.id_mesa);
+    if (mesa.length == 0) return res.status(400).send("Mesa inexistente");
+    mesa = mesa[0];
+
+    // validações
+    if (mesa.fechada) return res.status(400).send("Essa mesa já foi fechada");
+    if (mesa.encerrada) return res.status(400).send("Essa mesa já foi encerrada");
+
+    // obtendo os dados dos produtos
+    let produtos = [];
+    for (p of obj.produtos) {
+      // validando o item
+      let errors = model.validarItem(p);
+      if (errors)
+        return res.status(400).send(errors[0]);
+      // obtendo os dados do produto
+      let produto = await produtoCtrl._obter(req.token.id_restaurante, p.id_produto);
+      if (!produto || produto.length == 0)
+        return res.status(400).send("produto(s) inválido(s)");
+      else {
+        produto = produto[0];
+        // inclui a quantidade e atribui um id e dados da inserção
+        produto.quantidade = p.quantidade;
+        produto.id_item = new ObjectId();
+        produto.data_incluiu = new Date();
+        produto.id_operador = operador[0].id_operador;
+        produto.nome_operador = operador[0].nome_operador;
+        produtos.push(produto);
+      }
+    }
+
+    // altera os dados
+    if (!mesa.produtos) mesa.produtos = [];
+    mesa.produtos = mesa.produtos.concat(produtos);
+
+    // atualiza
+    await mongodb.replaceOne('freeddb', 'mesa', {
+      _id: new ObjectId(obj.id_mesa),
+      id_restaurante: req.token.id_restaurante
+    }, mesa);
+
+    return res.json('OK');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 }
 
 module.exports.remover = async (req, res) => {
-    try {
+  try {
 
-        let obj = {
-            id_mesa: req.body.id_mesa,
-            id_item: req.body.id_item,
-        }
-
-        let errors = model.validarRemover(obj);
-        if (errors)
-            return res.status(400).send(errors[0]);
-
-        await mongodb.updateOne('freeddb', 'mesa',
-            {
-                _id: new ObjectId(obj.id_mesa),
-                id_restaurante: req.token.id_restaurante
-            },
-            {
-                $set: {
-                    'produtos.$[item].removido': true,
-                    'produtos.$[item].data_removido': new Date(),
-                }
-            },
-            {
-                arrayFilters: [{ 'item.id_item': new ObjectId(obj.id_item) }]
-            }
-        );
-
-        return res.json('OK');
-    } catch (error) {
-        return res.status(500).send(error.message);
+    let obj = {
+      id_mesa: req.body.id_mesa,
+      id_item: req.body.id_item,
     }
+
+    let errors = model.validarRemover(obj);
+    if (errors)
+      return res.status(400).send(errors[0]);
+
+    // obtem mesa
+    let mesa = await mesaCtrl._obter(req.token.id_restaurante, req.body.id_mesa);
+    if (mesa.length == 0) return res.status(400).send("Mesa inexistente");
+    mesa = mesa[0];
+
+    // validações
+    if (mesa.fechada) return res.status(400).send("Essa mesa já foi fechada");
+    if (mesa.encerrada) return res.status(400).send("Essa mesa já foi encerrada");
+
+    // obtem o item
+    let item = mesa.produtos.find(p => p.id_item == req.body.id_item);
+
+    // altera os dados
+    item.removido = true;
+    item.data_removeu = new Date();
+
+    // atualiza
+    await mongodb.replaceOne('freeddb', 'mesa', {
+      _id: new ObjectId(obj.id_mesa),
+      id_restaurante: req.token.id_restaurante
+    }, mesa);
+
+    return res.json('OK');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 }
--- controllers/pagamento.controller.js
+++ controllers/pagamento.controller.js
@@ -3,6 +3,7 @@ const ObjectId = require('../utils/mongodb.util').ObjectId;
 const model = require('../models/pagamento.model');
 const restauranteCtrl = require('../controllers/restaurante.controller');
 const operadorCtrl = require('../controllers/operador.controller');
+const mesaCtrl = require('../controllers/mesa.controller');
 
 module.exports.incluir = async (req, res) => {
   try {
@@ -18,12 +19,22 @@ module.exports.incluir = async (req, res) => {
     if (errors)
       return res.status(400).send(errors[0]);
     if (!obj.pagamentos || obj.pagamentos.length == 0)
-      return res.status(400).send("Não há produtos na lista");
+      return res.status(400).send("Não há pagamentos na lista");
 
+    // valida operador
     let operador = await operadorCtrl._obter(req.token.id_restaurante, obj.id_operador);
     if (operador.length == 0)
       return res.status(400).send("Operador inválido");
 
+    // obtem mesa
+    let mesa = await mesaCtrl._obter(req.token.id_restaurante, req.body.id_mesa);
+    if (mesa.length == 0) return res.status(400).send("Mesa inexistente");
+    mesa = mesa[0];
+
+    // validações
+    if (mesa.encerrada) return res.status(400).send("Essa mesa já foi encerrada");
+
+    // prepara lista de pagamentos
     let pagamentos = [];
     for (p of obj.pagamentos) {
       // validando o pagamento
@@ -37,18 +48,22 @@ module.exports.incluir = async (req, res) => {
       else {
         p.ds_forma_pagamento = formaPgto[0].ds_forma_pagamento;
         p.id_pagamento = new ObjectId();
-        p.data_inclusao = new Date();
+        p.data_incluiu = new Date();
         p.id_operador = operador[0].id_operador;
         p.nome_operador = operador[0].nome_operador;
         pagamentos.push(p);
       }
     }
 
-    // incluindo os pagamentos na mesa
-    await mongodb.updateOne('freeddb', 'mesa',
-      { _id: new ObjectId(obj.id_mesa), id_restaurante: req.token.id_restaurante },
-      { $push: { pagamentos: { $each: pagamentos } } }
-    );
+    // altera os dados
+    if (!mesa.pagamentos) mesa.pagamentos = [];
+    mesa.pagamentos = mesa.pagamentos.concat(pagamentos);
+
+    // atualiza
+    await mongodb.replaceOne('freeddb', 'mesa', {
+      _id: new ObjectId(obj.id_mesa),
+      id_restaurante: req.token.id_restaurante
+    }, mesa);
 
     return res.json('OK');
   } catch (error) {
@@ -68,21 +83,26 @@ module.exports.remover = async (req, res) => {
     if (errors)
       return res.status(400).send(errors[0]);
 
-    await mongodb.updateOne('freeddb', 'mesa',
-      {
-        _id: new ObjectId(obj.id_mesa),
-        id_restaurante: req.token.id_restaurante
-      },
-      {
-        $set: {
-          'pagamentos.$[pagamento].removido': true,
-          'pagamentos.$[pagamento].data_removido': new Date(),
-        }
-      },
-      {
-        arrayFilters: [{ 'pagamento.id_pagamento': new ObjectId(obj.id_pagamento) }]
-      }
-    );
+    // obtem mesa
+    let mesa = await mesaCtrl._obter(req.token.id_restaurante, req.body.id_mesa);
+    if (mesa.length == 0) return res.status(400).send("Mesa inexistente");
+    mesa = mesa[0];
+
+    // validações
+    if (mesa.encerrada) return res.status(400).send("Essa mesa já foi encerrada");
+
+    // obtem o pagamento
+    let pagamento = mesa.pagamentos.find(p => p.id_pagamento == req.body.id_pagamento);
+
+    // altera os dados
+    pagamento.removido = true;
+    pagamento.data_removeu = new Date();
+
+    // atualiza
+    await mongodb.replaceOne('freeddb', 'mesa', {
+      _id: new ObjectId(obj.id_mesa),
+      id_restaurante: req.token.id_restaurante
+    }, mesa);
 
     return res.json('OK');
   } catch (error) {
