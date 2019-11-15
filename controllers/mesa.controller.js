2b3a8d4fdc9b5bf6ae5aa114f740e82d30be63d1
iagoguimaraes <iagog6@hotmail.com>
2019-11-15 17:17:09 -0300

    ajuste filtro mesas não encerradas

--- controllers/mesa.controller.js
+++ controllers/mesa.controller.js
@@ -9,7 +9,7 @@ module.exports.listar = async (req, res) => {
 
     let data = await mongodb.find('freeddb', 'mesa', {
       id_restaurante: req.token.id_restaurante,
-      encerrada: false,
+      encerrada: { $exists: false },
     });
 
     return res.json(data);
